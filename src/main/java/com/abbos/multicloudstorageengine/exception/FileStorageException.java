94f390d861523c007e0f6d5f94c100000669ea03
Aliabbos <aliabbosashurov@gmail.com>
2025-02-09 20:00:56 +0500

    doc

--- src/main/java/com/abbos/multicloudstorageengine/exception/FileStorageException.java
+++ src/main/java/com/abbos/multicloudstorageengine/exception/FileStorageException.java
@@ -1,36 +1,16 @@
 package com.abbos.multicloudstorageengine.exception;
 
 /**
- * <p>
- * A custom runtime exception representing errors that occur during file storage operations.
- * This serves as the base exception class for all file storage-related errors in the framework.
- * </p>
- *
- * <p>
- * Subclasses of this exception provide more specific error handling for scenarios such as invalid
- * providers or missing configurations.
- * </p>
+ * Custom runtime exception for file storage errors.
  *
  * @author Aliabbos Ashurov
- * @since 08/February/2025 14:52
+ * * @since 08/February/2025 14:52
  */
 public class FileStorageException extends RuntimeException {
-
-    /**
-     * Constructs a new {@link FileStorageException} with the specified detail message.
-     *
-     * @param message the detail message describing the cause of the exception.
-     */
     public FileStorageException(String message) {
         super(message);
     }
 
-    /**
-     * Constructs a new {@link FileStorageException} with the specified detail message and cause.
-     *
-     * @param message the detail message describing the cause of the exception.
-     * @param cause   the underlying cause of the exception.
-     */
     public FileStorageException(String message, Throwable cause) {
         super(message, cause);
     }
